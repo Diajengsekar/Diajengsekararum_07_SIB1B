f3c24e2f3caa01891a8cf12eb8036569605659ab
diajengsekararum31@gmail.com <diajengsekararum31@gmail.com>
2023-09-21 13:20:00 +0700

    Jobshee 4, "Percobaan 3, No 2"

---
+++ HargaBayar07.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+    public class HargaBayar07{
+    public static void main(String[] args){
+        
+        Scanner input =new Scanner(System.in);
+
+        int harga, jumlah;
+        double dis=0.1, total, bayar, jmlDis;
+
+        System.out.println("Masukkan harga bareang yang dibeli");
+        harga=input.nextInt();
+        System.out.println("Masukkan Jumlah jumlah barang yang dbeli");
+        jumlah=input.nextInt();
+
+        total=harga*jumlah;
+        jmlDis=total*dis;
+        bayar=total-jmlDis;
+
+        System.out.println("Diskon yang anda dapatkan adalah" +jmlDis);
+        System.out.println("Jumlah yang harus dibayar adalah" +bayar);
+
+    }
+
+}
